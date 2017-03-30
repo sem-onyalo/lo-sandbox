16af7ed9d8333de1c9d2c98a50a2514ccd6d4d27
sem_onyalo <sem.onyalo@stericycle.com>
2017-03-29 22:02:14 -0400

    added parameters for build properties to CI script

--- LoyaltyOne.CI/script.ps1
+++ LoyaltyOne.CI/script.ps1
@@ -1,5 +1,12 @@
+param(
+	[string]$workspacePath = "..",
+	[string]$deployOnBuild = "false",
+	[string]$deployProfile = "dev-local",
+	[string]$vsVersion = "14.0"
+)
+
 $buildCmd = "msbuild"
-$buildSourcePath = "..\LoyaltyOne\LoyaltyOne.Web\LoyaltyOne.Web.csproj"
-$buildCmdProperties = ("/property:DeployOnBuild={0};PublishProfile={1};VisualStudioVersion={2}" -f "true", "dev-local", "14.0")
+$buildSourcePath = ("{0}\{1}" -f  $workspacePath, "LoyaltyOne\LoyaltyOne.Web\LoyaltyOne.Web.csproj") 
+$buildCmdProperties = ("/property:DeployOnBuild={0};PublishProfile={1};VisualStudioVersion={2}" -f $deployOnBuild, $deployProfile, $vsVersion)
 
 & $buildCmd $buildSourcePath $buildCmdProperties
